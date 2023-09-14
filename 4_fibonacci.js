97d3bce16847a0e185334ad8766ac04a0ffb432c
koushikwebdev <kou.saha.ami@gmail.com>
2023-09-14 23:40:37 +0530

    added new fibonacci pblm

--- 4_fibonacci.js
+++ 4_fibonacci.js
@@ -15,12 +15,13 @@ let fibonacci = (n) => {
 console.log(fibonacci(7));
 
 function fibo(num) {
-  if (num < 2) return num;
+  if (num == 0) return 6;
+  if (num == 1) return 7;
 
   return fibo(num - 1) + fibo(num - 2);
 }
 
-// console.log(fibo(7));
+console.log("koushik", fibo(3));
 
 const fibonacciSequence = (num) => {
   let fibo = [0, 1];
@@ -33,3 +34,16 @@ const fibonacciSequence = (num) => {
 };
 
 console.log(fibonacciSequence(7));
+
+function fibonacciRecursive(n, a = 6, b = 7) {
+  if (n <= 0) {
+    return a;
+  } else {
+    return fibonacciRecursive(n - 1, b, a + b);
+  }
+}
+
+// Example usage:
+const n = 10; // Change this to the desired Fibonacci number you want to find.
+const result = fibonacciRecursive(n, 6, 7);
+console.log(`The ${n}-th Fibonacci number is: ${result}`);
