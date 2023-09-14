696bdaf9538c6ae44494a5765c23d9cf49117337
koushikwebdev <kou.saha.ami@gmail.com>
2023-09-14 23:40:46 +0530

    none

--- 7_bubbleSort.js
+++ 7_bubbleSort.js
@@ -16,4 +16,21 @@ const bubbleSort = (arr) => {
   return arr;
 };
 
-console.log(bubbleSort(arr)); //big O = n^2
+// console.log(bubbleSort(arr)); //big O = n^2
+
+const bubbleSortReverse = (arr) => {
+  if (!arr.length) return;
+  let i = 0,
+    j = arr.length - 1;
+
+  while (i < j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+
+    i++;
+    j--;
+  }
+  return arr;
+};
+console.log(bubbleSortReverse(arr));
